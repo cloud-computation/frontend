04c1813eb93fd1f2c69a04c7d2ea7588e6287904
Maksim <maxim.kudryavtsev@omega-r.com>
2020-04-02 18:46:22 +0300

    add components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,6 @@ export * from "./private-route";
 export * from "./public-route";
 export * from "./text-field";
 export * from "./jss-provider";
+export * from "./popup";
+export * from "./menu";
+export * from "./confirm-popup";
